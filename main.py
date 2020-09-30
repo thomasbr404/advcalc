335bba1f12a767544a62a99354b2102f8c838903
thomasbr404 <71598048+thomasbr404@users.noreply.github.com>
2020-09-30 16:44:32 -0400

    a/s/m/d execution
    + executes
    - executes
    * executes
    / executes

--- main.py
+++ main.py
@@ -46,13 +46,27 @@ def t_error(t):
 	
 lexer = lex.lex()
 
+precedence = (
+	('left', 'PLUS', 'MINUS'),
+	('left', 'MULTIPLY', 'DIVIDE')
+)
+
+
 def p_calc(p):
 	'''
 	calc : expression
+		 | var_assign
 		 | empty
 	'''
-	print(p[1])
+	print(run(p[1]))
 	
+def p_var_assign(p):
+	'''
+	var_assign : NAME EQUALS expression
+	'''
+	p[0] = ('=', p[1], p[3])
+
+
 def p_expression(p):
 	'''
 	expression : expression MULTIPLY expression
@@ -68,7 +82,16 @@ def p_expression_int_float(p):
 			   | FLOAT
 	'''
 	p[0] = p[1]
+
+def p_expression_var(p):
+	'''
+	expression : NAME
+	'''
+	p[0] = ('var', p[1])
 	
+def p_error(p):
+	print("Syntax error!")
+
 def p_empty(p):
 	'''
 	empty :
@@ -77,9 +100,22 @@ def p_empty(p):
 	
 parser = yacc.yacc()
 
+def run(p):
+	if type(p) == tuple:
+		if p[0] == '+':
+			return run(p[1]) + run(p[2])
+		elif p[0] == '-':
+			return run(p[1]) - run(p[2])
+		elif p[0] == '*':
+			return run(p[1]) * run(p[2])
+		elif p[0] == '/':
+			return run(p[1]) / run(p[2])
+	else:
+		return p
+
 while True:
 	try:
-		s = input('')
+		s = input('> ')
 	except EOFError:
 		break
 	parser.parse(s)
